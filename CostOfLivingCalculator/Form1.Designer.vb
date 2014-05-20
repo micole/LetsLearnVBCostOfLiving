c53112db1211f1be2b292358d9142b0aecc10b68
Michael Cole <micole.3@gmail.com>
2014-05-20 00:59:23 -0400

    First pass of using tax information and wages

--- CostOfLivingCalculator/Form1.Designer.vb
+++ CostOfLivingCalculator/Form1.Designer.vb
@@ -24,27 +24,87 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Test1 = New System.Windows.Forms.TextBox()
         Me.Doubled = New System.Windows.Forms.TextBox()
+        Me.LWages = New System.Windows.Forms.Label()
+        Me.LTaxes = New System.Windows.Forms.Label()
+        Me.MWages = New System.Windows.Forms.TextBox()
+        Me.Taxes = New System.Windows.Forms.TextBox()
+        Me.LDiscretionary = New System.Windows.Forms.Label()
+        Me.DiscretionaryIncome = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Test1
         '
-        Me.Test1.Location = New System.Drawing.Point(12, 81)
+        Me.Test1.Location = New System.Drawing.Point(238, 43)
         Me.Test1.Name = "Test1"
         Me.Test1.Size = New System.Drawing.Size(100, 20)
         Me.Test1.TabIndex = 0
         '
         'Doubled
         '
-        Me.Doubled.Location = New System.Drawing.Point(157, 81)
+        Me.Doubled.Location = New System.Drawing.Point(389, 81)
         Me.Doubled.Name = "Doubled"
         Me.Doubled.Size = New System.Drawing.Size(100, 20)
         Me.Doubled.TabIndex = 1
+        '
+        'LWages
+        '
+        Me.LWages.AutoSize = True
+        Me.LWages.Location = New System.Drawing.Point(82, 88)
+        Me.LWages.Name = "LWages"
+        Me.LWages.Size = New System.Drawing.Size(84, 13)
+        Me.LWages.TabIndex = 2
+        Me.LWages.Text = "Monthly Wages:"
+        '
+        'LTaxes
+        '
+        Me.LTaxes.AutoSize = True
+        Me.LTaxes.Location = New System.Drawing.Point(60, 118)
+        Me.LTaxes.Name = "LTaxes"
+        Me.LTaxes.Size = New System.Drawing.Size(106, 13)
+        Me.LTaxes.TabIndex = 3
+        Me.LTaxes.Text = "Taxes (28% normally)"
+        '
+        'MWages
+        '
+        Me.MWages.Location = New System.Drawing.Point(172, 85)
+        Me.MWages.Name = "MWages"
+        Me.MWages.Size = New System.Drawing.Size(100, 20)
+        Me.MWages.TabIndex = 4
+        '
+        'Taxes
+        '
+        Me.Taxes.Location = New System.Drawing.Point(172, 115)
+        Me.Taxes.Name = "Taxes"
+        Me.Taxes.Size = New System.Drawing.Size(100, 20)
+        Me.Taxes.TabIndex = 5
+        '
+        'LDiscretionary
+        '
+        Me.LDiscretionary.AutoSize = True
+        Me.LDiscretionary.Location = New System.Drawing.Point(17, 622)
+        Me.LDiscretionary.Name = "LDiscretionary"
+        Me.LDiscretionary.Size = New System.Drawing.Size(149, 13)
+        Me.LDiscretionary.TabIndex = 6
+        Me.LDiscretionary.Text = "Monthly Discretionary Income:"
+        '
+        'DiscretionaryIncome
+        '
+        Me.DiscretionaryIncome.Location = New System.Drawing.Point(172, 619)
+        Me.DiscretionaryIncome.Name = "DiscretionaryIncome"
+        Me.DiscretionaryIncome.Size = New System.Drawing.Size(100, 20)
+        Me.DiscretionaryIncome.TabIndex = 7
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(501, 705)
+        Me.Controls.Add(Me.DiscretionaryIncome)
+        Me.Controls.Add(Me.LDiscretionary)
+        Me.Controls.Add(Me.Taxes)
+        Me.Controls.Add(Me.MWages)
+        Me.Controls.Add(Me.LTaxes)
+        Me.Controls.Add(Me.LWages)
         Me.Controls.Add(Me.Doubled)
         Me.Controls.Add(Me.Test1)
         Me.Name = "Form1"
@@ -55,5 +115,11 @@ Partial Class Form1
     End Sub
     Friend WithEvents Test1 As System.Windows.Forms.TextBox
     Friend WithEvents Doubled As System.Windows.Forms.TextBox
+    Friend WithEvents LWages As System.Windows.Forms.Label
+    Friend WithEvents LTaxes As System.Windows.Forms.Label
+    Friend WithEvents MWages As System.Windows.Forms.TextBox
+    Friend WithEvents Taxes As System.Windows.Forms.TextBox
+    Friend WithEvents LDiscretionary As System.Windows.Forms.Label
+    Friend WithEvents DiscretionaryIncome As System.Windows.Forms.TextBox
 
 End Class
--- CostOfLivingCalculator/Form1.vb
+++ CostOfLivingCalculator/Form1.vb
@@ -5,9 +5,37 @@
     End Sub
 
     Private Sub UpdateNumbers()
-        Dim x = Integer.Parse(Test1.Text)
-        x = x + x
-        Doubled.Text = x
 
+        'If IsNumeric(Test1.Text) Then
+        '    Dim x = Integer.Parse(Test1.Text)
+        '    x = x + x
+        '    Doubled.Text = x
+        'Else
+        '    Doubled.Text = "NaN"
+        'End If
+        If IsNumeric(MWages.Text) And IsNumeric(Taxes.Text) Then
+            Dim tax = Double.Parse(Taxes.Text)
+            If tax > 0 And tax < 100 Then
+                tax = 100 - tax
+                tax = tax * 0.01
+            Else
+                tax = 0.28
+                Taxes.Text = "28"
+            End If
+            Doubled.Text = tax
+            DiscretionaryIncome.Text = (Integer.Parse(MWages.Text) * tax)
+        Else
+            DiscretionaryIncome.Text = "NaN"
+        End If
+
+
+    End Sub
+
+    Private Sub MWages_TextChanged(sender As Object, e As EventArgs) Handles MWages.TextChanged
+        UpdateNumbers()
+    End Sub
+
+    Private Sub Taxes_TextChanged(sender As Object, e As EventArgs) Handles Taxes.TextChanged
+        UpdateNumbers()
     End Sub
 End Class
